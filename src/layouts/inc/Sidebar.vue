1277d8b0900988ce1be24569684a55e6ba048ef4
4dayman <109858025+4dayman@users.noreply.github.com>
2023-04-05 11:45:46 +0300

    commit

--- src/layouts/inc/Sidebar.vue
+++ src/layouts/inc/Sidebar.vue
@@ -2,7 +2,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
         <a href="/" class="brand-link">
-            <img src="/adminlte/dist/img/AdminLTELogo.png" :alt="this.$store.state.app.name" class="brand-image img-circle elevation-3"
+            <img src="@/assets/AdminLTELogo.png" :alt="this.$store.state.app.name" class="brand-image img-circle elevation-3"
                 style="opacity: .8">
             <span class="brand-text font-weight-light">{{ this.$store.state.app.name }}</span>
         </a>
@@ -11,7 +11,7 @@
 
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <!-- <img src="/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> -->
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">Alexander Pierce</a>
